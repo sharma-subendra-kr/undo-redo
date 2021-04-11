383f299969f730942fe3ff3d85a5bf6bfa10b5a9
Subendra Sharma <32413952+sharma-subendra-kr@users.noreply.github.com>
2021-04-11 07:58:24 +0530

    update

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,23 @@
 /*
 
-undo-redo-stack, a undo redo stack data structure.
+undo-redo, a undo redo stack data structure.
 
 Copyright © 2020-2021 Subendra Kumar Sharma. All rights reserved. (jobs.sharma.subendra.kr@gmail.com)
 
-This file is part of undo-redo-stack.
+This file is part of undo-redo.
 
-undo-redo-stack is free software: you can redistribute it and/or modify
+undo-redo is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-undo-redo-stack is distributed in the hope that it will be useful,
+undo-redo is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with undo-redo-stack.  If not, see <https://www.gnu.org/licenses/>.
+along with undo-redo.  If not, see <https://www.gnu.org/licenses/>.
 
 Written by Subendra Kumar Sharma.
 
